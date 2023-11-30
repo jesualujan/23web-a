0c40f92eae15d36a33ca113a27082a677afc2e23
jesualujan <lujan.oficial@gmail.com>
2023-11-29 20:05:22 -0600

    ✨ creamos nuestra conexión a la bd de mongo

--- 01-Docker/index.js
+++ 01-Docker/index.js
@@ -1,38 +1,45 @@
-//* va nuestro código (servidor)
 const express = require ('express')
 const mongoose = require ('mongoose')
-const colors = require ('colors')
-const port = 3000;
+const colors = require('colors')
+const port = 3003;
 
+const Animal = mongoose.model('Animal', new mongoose.Schema({
+  tipo: String,
+  estado: String,
+}))
 
-const app = express ()
+const app = express()
 
-// Mensaje de bienvenida
-app.get('/', async(req,res) => {
-    res.setHeader('Content-Type', 'text/html')
-    res.status(200)
-    res.send('<h1>ESTÁ FUNCIONANDO</h1>')
+mongoose.connect('mongodb://jesua:password@monguitodos:27017/miapp?authSource=admin')
+
+//mensaje de bienvenida
+app.get('/', async (req, res) => {
+    res.setHeader("Content-Type", "text/html");
+    res.status(200);
+    res.send("<h1>SI FUNCIONA</h1>");
 })
 
-// Buscar un animalito (listar)
-app.get('/listar', async (req,res)=>{
-    console.log('listando ... cochinitos...')
-    const animales = await Animal.find()
+// buscar un animal
+app.get('/listar', async (req, res) => {
+    console.log('listando... cochinitos...')
+    const animales = await Animal.find();
     return res.send(animales)
-})
+  })
 
-// Crear un animalito
-app.get('/crear', async(req,res)=>{
-    console.log('creando....')
-    await Animal.create({tipo: 'Cochinito', estado: 'Feliz'})
-    await Animal.create({tipo: 'Gatito', estado: 'Programando'})
-    await Animal.create({tipo: 'Perrito', estado: 'Comiendo'})
-    await Animal.create({tipo: 'Conejito', estado: 'Saltando'})
-    res.status(200)
+// crear un animal
+app.get('/crear', async (req, res) => {
+    console.log('creando...')
+    await Animal.create({ tipo: 'Cochinito', estado: 'Feliz' })
+    await Animal.create({ tipo: 'Gatito', estado: 'Programando'})
+    await Animal.create({ tipo: 'Perrito', estado: 'Comiendo' })
+    await Animal.create({ tipo: 'Conejito', estado: 'Saltando' })
+    res.status(200);
     return res.send('Created...')
-})
+  })
 
-// habilitar el puerto/servidor
+// app.listen(3000, () => console.log('listening...'.bgCyan))
+// https://www.npmjs.com/package/colors
+
 app.listen(port, ()=> {
     console.log("Servidor corriendo en el puerto:".bgBlue + " " + `http://localhost:${port}`.underline)
 })
